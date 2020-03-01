04e56091a8e950c6fb789fa27a2fb7c99ae800e5
AlvarTapia <alvar.tapia@alumnos.unican.es>
2020-03-01 17:03:26 +0100

    Se pueden copiar tableros. Peleandome con mov fichas.

--- ReglasDamas.py
+++ ReglasDamas.py
@@ -13,7 +13,7 @@ def posicionValida(x, y, longTablero):
 
     return True
 
-def ponFichasIniciales(longTablero=8, relacionCasillasFichas=0.4, equipos=("Blanco", "Negro")):
+def tableroConFichasIniciales(longTablero=8, relacionCasillasFichas=0.4, equipos=("Blanco", "Negro")):
     t = Tablero(longTablero, equipos)
     filasPeones = int(longTablero*relacionCasillasFichas)
 
@@ -31,18 +31,63 @@ def ponFichasIniciales(longTablero=8, relacionCasillasFichas=0.4, equipos=("Blan
         for y in range(inicial, final+1): #+1 porque "final" es el ultimo valor que queremos rellenar
             for x in iter(x for x in range(t.LONG_TABLERO) if posicionValida(x, y, t.LONG_TABLERO)):
                 t.fichasDelEquipo[e][(x, y)] = t.PEON
-                print("Ficha puesta")
     return t
 
-def movimientosFichaEnTablero(x, y, tablero):
-    pass
+def movimientosPosiblesFichaEnTablero(x, y, tablero):
+    EH = []
+    if not posicionValida(x, y, tablero.LONG_TABLERO):
+        return EH
 
+    equipo = tablero.equipoEnCoordenadas(x, y)
+    if not equipo:
+        return EH
+
+    ficha = tablero.fichasDelEquipo[equipo][ (x, y) ]
+
+    dirFicha = -1 if tablero.filaObjetivoDelEquipo[equipo] - y < 0 else 1
+
+    movPosibles = []
+    for i in range(-ficha.movMax, ficha.movMax + 1): #+1 para abarcar movMax
+        if i == 0:
+            continue
+
+        if not ficha.puedeIrAtras:
+            dirMovY = -1 if i < 0 else 1
+            if dirFicha != dirMovY:
+                continue
+
+        posibleY = y + i
+        for posibleX in (x - i, x + i):
+            if posicionValida(posibleX, posibleY, tablero.LONG_TABLERO):
+                movPosibles.append( (posibleX, posibleY) )
+
+
+    return movPosibles
+
+def movimientosLegalesFichaEnTablero(x, y, tablero):
+    ## TODO:
+    pass
+    
 class PruebasReglas(unittest.TestCase):
     def setUp(self):
-        self.t = ponFichasIniciales()
+        self.t = tableroConFichasIniciales()
 
     def testFichasInicialesBienPuestas(self):
+        for e in self.t.fichasDelEquipo:
+            self.assertEqual(len(self.t.fichasDelEquipo[e]), 12)
+
+    def testFichasPorEquipo(self):
+        for e in self.t.EQUIPOS:
+            self.assertEqual(self.t.fichasPorEquipo(e), 12)
+        self.assertEqual(self.t.fichasPorEquipo("Random"), -1)
+        self.assertEqual(self.t.fichasPorEquipo(3), -1)
+
+    def testMovPosibles(self):
         print(self.t)
+        for e in self.t.fichasDelEquipo:
+            for x, y in self.t.fichasDelEquipo[e]:
+                print(e, (x, y), movimientosFichaEnTablero(x, y, self.t))
+
 
 if __name__ == "__main__":
     unittest.main()
--- Tablero.py
+++ Tablero.py
@@ -17,8 +17,13 @@ class Tablero():
         for e, fichasEquipo in self.fichasDelEquipo.items():
             if (x, y) in fichasEquipo:
                 return e
-        else:
+        return EH
+
+    def fichasPorEquipo(self, equipo):
+        EH = -1
+        if equipo not in self.EQUIPOS:
             return EH
+        return len(self.fichasDelEquipo[equipo])
 
     def __str__(self):
         string = "\n"
@@ -43,6 +48,36 @@ class Tablero():
         string = "_" * (2*self.LONG_TABLERO + 1) + string
 
         return string
+
+    def __copy__(self):
+        t = Tablero(self.LONG_TABLERO, self.EQUIPOS)
+        t.PEON = self.PEON
+        t.DAMA = self.DAMA
+
+        t.filaObjetivoDelEquipo = self.filaObjetivoDelEquipo.copy()
+        t.fichasDelEquipo = self.fichasDelEquipo.copy()
+        return t
+
+    def copia(self):
+        return self.__copy__()
+
+class PruebasTablero(unittest.TestCase):
+    def setUp(self):
+        self.t = Tablero()
+
+    def testCopia(self):
+        self.t.fichasDelEquipo["Blanco"][(0, 0)] = self.t.PEON
+        self.t.fichasDelEquipo["Negro"][(1, 1)] = self.t.PEON
+
+        copia = self.t.copia()
+        self.t.fichasDelEquipo["Blanco"] = {}
+        self.t.fichasDelEquipo["Negro"] = {}
+        for e in self.t.fichasDelEquipo:
+            self.assertEqual(len(self.t.fichasDelEquipo[e]), 0)
+
+        for e in copia.fichasDelEquipo:
+            self.assertEqual(len(copia.fichasDelEquipo[e]), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
